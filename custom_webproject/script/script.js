598da5612e80773f44c4361c2ef083de94548537
vassmate <vassmate08@gmail.com>
2016-05-25 23:30:00 +0200

    Refined input validation and added warnings

--- custom_webproject/script/script.js
+++ custom_webproject/script/script.js
@@ -5,21 +5,48 @@
     var submitButton = document.getElementById("submitButton");
     submitButton.disabled = true;
 
+    var nameDiv = document.getElementById("contact_name");
+    var emailDiv = document.getElementById("contact_email");
+    var messageDiv = document.getElementById("contact_msg");
+
+    var showWarning = function (divName) {
+        var span = document.createElement("span");
+        span.setAttribute("id", "span" + divName.id);
+        var node = document.createTextNode("Not valid input!");
+        span.appendChild(node);
+        divName.appendChild(span);
+    };
+
+    var removeWarning = function (divName) {
+        var span = document.getElementById("span" + divName.id);
+        if (span != null) {
+            divName.removeChild(span);
+        }
+    };
+
     var validateContactData = function () {
         var emailLength = email.value.length;
         var emailHu = email.value.substring((emailLength - 3), emailLength);
         var emailCom = email.value.substring((emailLength - 4), emailLength);
+        var isValidInputs = true;
+
+        removeWarning(nameDiv);
+        removeWarning(emailDiv);
+        removeWarning(messageDiv);
 
         if (name.value.trim() === "" || name.value.length < 3 || name.value.length > 30) {
-            return false;
+            showWarning(nameDiv);
+            isValidInputs = false;
         }
-        else if (email.value.trim() === "" || emailHu != ".hu" && emailCom != ".com") {
-            return false;
+        if (email.value.trim() === "" || emailHu != ".hu" && emailCom != ".com") {
+            showWarning(emailDiv);
+            isValidInputs = false;
         }
-        else if (email.value.trim() === "" || message.value.length < 20 || message.value.length > 500) {
-            return false;
+        if (email.value.trim() === "" || message.value.length < 20 || message.value.length > 500) {
+            showWarning(messageDiv);
+            isValidInputs = false;
         }
-        return true;
+        return isValidInputs;
     };
 
     var showMessage = function () {
@@ -29,6 +56,12 @@
             "\n" + "\t" + "Message: " + message.value;
 
         alert(siteMsg + inputMsg);
+
+        name.value = "";
+        email.value = "";
+        message.value = "";
+
+        submitButton.disabled = true;
     };
 
     var checkInputs = function () {
